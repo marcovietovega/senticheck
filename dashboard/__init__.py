e2d3cbd738921a8601fb8228e04275bef0aefe27
Marco Vieto <mv03790@gmail.com>
2025-08-31 00:24:46 +1200

    refactor(dashboard): remove chart utilities module and update imports

--- dashboard/__init__.py
+++ dashboard/__init__.py
@@ -11,12 +11,10 @@ __author__ = "SentiCheck Team"
 
 from .config import DASHBOARD_CONFIG, SENTIMENT_LABELS, KPI_METRICS
 from .data_service import get_dashboard_data_service
-from .chart_utils import create_sentiment_distribution_chart
 
 __all__ = [
     "DASHBOARD_CONFIG",
     "SENTIMENT_LABELS",
     "KPI_METRICS",
     "get_dashboard_data_service",
-    "create_sentiment_distribution_chart",
 ]
