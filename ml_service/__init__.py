110ec4e4ea6b34ae8414b3adb860b952617956f8
Marco Vieto <mv03790@gmail.com>
2025-09-15 00:15:01 +1200

    refactor: remove unused ML service files and classes to streamline the codebase

--- ml_service/__init__.py
+++
@@ -1,8 +0,0 @@
-# ML Service package for SentiCheck sentiment analysis API
-
-from .sentiment_analyzer import SentimentAnalyzer, analyze_sentiment_batch
-
-__all__ = [
-    "SentimentAnalyzer",
-    "analyze_sentiment_batch",
-]
