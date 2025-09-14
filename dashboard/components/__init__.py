6103126207f54730eed4aba03759a7d16b7a870d
Marco Vieto <mv03790@gmail.com>
2025-09-15 01:37:32 +1200

    refactor(dashboard): remove unused keyword selector component and update exports

--- dashboard/components/__init__.py
+++ dashboard/components/__init__.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
 
-from .keyword_selector import render_keyword_selector
 from .sidebar_controls import render_sidebar_controls, update_session_state_from_sidebar
 
 __all__ = [
-    "render_keyword_selector",
     "render_sidebar_controls",
     "update_session_state_from_sidebar",
 ]
