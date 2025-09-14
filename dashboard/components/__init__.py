67aa6b31a615b94051cb6d3d3caee07386ec7ce2
Marco Vieto <mv03790@gmail.com>
2025-09-15 00:14:38 +1200

    refactor(components): remove insights section from the dashboard component

--- dashboard/components/__init__.py
+++ dashboard/components/__init__.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3
 
 from .keyword_selector import render_keyword_selector
-from .insights_section import render_insights_section
 from .sidebar_controls import render_sidebar_controls, update_session_state_from_sidebar
 
 __all__ = [
     "render_keyword_selector",
-    "render_insights_section",
     "render_sidebar_controls",
     "update_session_state_from_sidebar",
 ]
