038b1413374279a973acd8b63b79d497cb950988
Marco Vieto <mv03790@gmail.com>
2025-09-16 01:38:24 +1200

    refactor(connectors): remove Bluesky connector files to simplify codebase

--- connectors/__init__.py
+++
@@ -1,7 +0,0 @@
-# Connectors package for SentiCheck
-
-from .bluesky.fetch_posts import BlueskyConnector
-
-__all__ = [
-    "BlueskyConnector",
-]
--- connectors/bluesky/__init__.py
+++
@@ -1,7 +0,0 @@
-# Bluesky connector module
-
-from .fetch_posts import BlueskyConnector
-
-__all__ = [
-    "BlueskyConnector",
-]
